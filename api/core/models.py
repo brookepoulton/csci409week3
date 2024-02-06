80edf51dd132477ae406f5b449720ee17c396e0c
brookepoulton <brookepoulton@icloud.com>
2024-02-06 11:02:48 -0500

    Created Runways Model

--- api/core/models.py
+++ api/core/models.py
@@ -10,3 +10,35 @@ class Airport(models.Model):
 
     def __str__(self): 
         return self.name
+    
+class Airline(models.Model): 
+    name =  models.CharField(max_length=255)
+    airline_code= models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+class Runway(models.Model):
+    class RunwayChoice(models.TextChoices): 
+        LEFT = 'L', 'Left'
+        CENTER = 'C', 'Center'
+        RIGHT = 'R', 'Right'
+        NONE = 'N', 'None'
+
+    runway_number = models.IntegerField()
+    runway_designation = models.CharField (
+        max_length=1,
+        choices = RunwayChoice.choices,
+        )
+    length = models.IntegerField() 
+    width = models.IntegerField()
+    airport = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name = 'runways'
+    )
+
+    def __str__(self):
+        return f'Runway: {self.runway_number}{self.runway_designation}'
+
+    
+
