f5434bf467e797423ae5e3b610008809ebe81785
brookepoulton <brookepoulton@icloud.com>
2024-02-08 08:58:45 -0500

    Added Runway Serializer

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from core.models import Airline
+from core.models import Airline, Runway
 
 class AirlineSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = Airline
         fields = ['name', 'airline_code']
+        read_only_fields = ['id'] 
+
+class RunwaySerializer(serializers.ModelSerializer): 
+    class Meta: 
+        model = Runway
+        fields = ['airport', 'runway_number', 'runway_designation', 'length', 'width']
+        read_only_fields = ['id']
